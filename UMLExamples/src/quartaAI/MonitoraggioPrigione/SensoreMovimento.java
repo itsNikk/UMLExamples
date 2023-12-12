1be2b9b4f14d0e06d8799ba78163396a398f9cec
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2023-12-12 12:26:31 +0100

    Added prison

---
+++ UMLExamples/src/quartaAI/MonitoraggioPrigione/SensoreMovimento.java
@@ -0,0 +1,23 @@
+package quartaAI.MonitoraggioPrigione;
+
+public class SensoreMovimento extends Sensore {
+
+    @Override
+    public void accendi() {
+
+    }
+
+    @Override
+    public void spegni() {
+
+    }
+
+    @Override
+    public void eseguiDiagnostica() {
+
+    }
+
+    public boolean rilevaMovimento(){
+        return false;
+    }
+}
---
+++ UMLExamples/src/quartaAI/MonitoraggioPrigione/SensoreTemperatura.java
@@ -0,0 +1,23 @@
+package quartaAI.MonitoraggioPrigione;
+
+public class SensoreTemperatura extends Sensore {
+
+    @Override
+    public void accendi() {
+
+    }
+
+    @Override
+    public void spegni() {
+
+    }
+
+    @Override
+    public void eseguiDiagnostica() {
+
+    }
+
+    public float rilevaTemperatura() {
+        return 0.0F;
+    }
+}
